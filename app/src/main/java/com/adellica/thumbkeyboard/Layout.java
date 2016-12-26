645c29b964197f639f52b74699f16a4f87924e85
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-12-26 17:16:13 +0100

    refac: move nested class Layout to Layout.java
    ThumbkeyboardView is becoming unnavigationable

---
+++ app/src/main/java/com/adellica/thumbkeyboard/Layout.java
@@ -0,0 +1,110 @@
+package com.adellica.thumbkeyboard;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+* Created by klm on 12/26/16.
+*/
+public class Layout {
+    public static final String TAG = "TKEY";
+
+    final public String name;
+    final Map<String, String> map;
+    public Layout(String name) {
+        this(name, new HashMap<String, String>());
+    }
+    public Layout(String name, Map<String, String> dict) {
+        this.name = name;
+        this.map = dict;
+    }
+
+    public String put(String key, String value) {
+        // obviously, we need to do this before we save:
+        final String result = map.put(key, value);
+
+        List<String> l = new ArrayList<String>(map.keySet());
+        Collections.sort(l); Collections.reverse(l);
+        String filename = ThumbkeyboardView.layoutname2path(name);
+        Log.i(TAG, "saving layout to " + filename);
+        try {
+            PrintWriter out = new PrintWriter(filename);
+            for(String stroke : l) {
+                out.println(stroke + map.get(stroke));
+                Log.i(TAG, "saved: " + stroke + map.get(stroke));
+            }
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String get(String key) {
+        return map.get(key);
+    }
+
+    public List<String> keys() {
+        List<String> l = new ArrayList<String>(map.keySet());
+        Collections.sort(l); Collections.reverse(l);
+        return l;
+    }
+
+    public static Layout fromFile(final String name) {
+        final String filename = ThumbkeyboardView.layoutname2path(name);
+        Log.i(TAG, "loading layout from file " + filename);
+        try {
+            String line;
+            Map<String, String> map = new HashMap<String, String>();
+            InputStream fis = new FileInputStream(filename);
+            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+            BufferedReader br = new BufferedReader(isr);
+
+            while ((line = br.readLine()) != null) {
+                final String pair[] = ThumbkeyboardView.Stroke.parse(line);
+                map.put(pair[0], pair[1]);
+            }
+            return new Layout(name, map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    static public Map<String, Layout> loadLayouts() {
+        Map<String, Layout> layouts = new HashMap<String, Layout>();
+
+        final File directory = new File(ThumbkeyboardView.layoutnamedir());
+        final File[] files = directory.listFiles();
+        Log.d(TAG, "Loading config files " + Arrays.asList(files));
+        for (int i = 0; i < files.length; i++)
+        {
+            final String filename = files[i].getName();
+            if(filename.endsWith(".chords")) {
+                final String name = filename.substring(0, filename.length() - 7);
+                Log.i(TAG, "loading chords file " + ThumbkeyboardView.layoutname2path(name) + " as " + name);
+                final Layout layout = fromFile(name);
+                layouts.put(name, layout);
+            }
+        }
+
+        return layouts;
+    }
+
+}
--- app/src/main/java/com/adellica/thumbkeyboard/ThumbkeyboardView.java
+++ app/src/main/java/com/adellica/thumbkeyboard/ThumbkeyboardView.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.os.Environment;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -18,20 +17,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -400,6 +387,9 @@ public class ThumbkeyboardView extends View {
             } else {
                 Log.i(TAG, "Don't know how to handle " + t);
             }
+        } else if("layout".equals(cmd)){
+            final String layout_s = value(t);
+
         } else if("delete".equals(cmd)) {
             if("line".equals(value(t))) {
                 final String preline = readBackwardsUntil("\n", true);
@@ -527,92 +517,6 @@ public class ThumbkeyboardView extends View {
         final String dir = layoutnamedir();
         new File(dir).mkdirs();
         return dir + name + ".chords";
-    }
-
-    public static class Layout {
-        final public String name;
-        final Map<String, String> map;
-        public Layout(String name) {
-            this(name, new HashMap<String, String>());
-        }
-        public Layout(String name, Map<String, String> dict) {
-            this.name = name;
-            this.map = dict;
-        }
-
-        public String put(String key, String value) {
-            // obviously, we need to do this before we save:
-            final String result = map.put(key, value);
-
-            List<String> l = new ArrayList<String>(map.keySet());
-            Collections.sort(l); Collections.reverse(l);
-            String filename = layoutname2path(name);
-            Log.i(TAG, "saving layout to " + filename);
-            try {
-                PrintWriter out = new PrintWriter(filename);
-                for(String stroke : l) {
-                    out.println(stroke + map.get(stroke));
-                    Log.i(TAG, "saved: " + stroke + map.get(stroke));
-                }
-                out.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            return result;
-        }
-
-        public String get(String key) {
-            return map.get(key);
-        }
-
-        public List<String> keys() {
-            List<String> l = new ArrayList<String>(map.keySet());
-            Collections.sort(l); Collections.reverse(l);
-            return l;
-        }
-
-        public static Layout fromFile(final String name) {
-            final String filename = layoutname2path(name);
-            Log.i(TAG, "loading layout from file " + filename);
-            try {
-                String line;
-                Map<String, String> map = new HashMap<String, String>();
-                InputStream fis = new FileInputStream(filename);
-                InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-                BufferedReader br = new BufferedReader(isr);
-
-                while ((line = br.readLine()) != null) {
-                    final String pair[] = Stroke.parse(line);
-                    map.put(pair[0], pair[1]);
-                }
-                return new Layout(name, map);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-
-        static public Map<String, Layout> loadLayouts() {
-            Map<String, Layout> layouts = new HashMap<String, Layout>();
-
-            final File directory = new File(layoutnamedir());
-            final File[] files = directory.listFiles();
-            Log.d(TAG, "Loading config files " + Arrays.asList(files));
-            for (int i = 0; i < files.length; i++)
-            {
-                final String filename = files[i].getName();
-                if(filename.endsWith(".chords")) {
-                    final String name = filename.substring(0, filename.length() - 7);
-                    Log.i(TAG, "loading chords file " + layoutname2path(name) + " as " + name);
-                    final Layout layout = fromFile(name);
-                    layouts.put(name, layout);
-                }
-            }
-
-            return layouts;
-        }
-
     }
 
     Map<String, Layout> layouts = new HashMap<String, Layout>();
