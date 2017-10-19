c73e832a894a3c166c5d0437735991b5e411e099
Kristian Lein-Mathisen <kristianlein@gmail.com>
2017-10-19 19:54:04 +0200

    whohooo now things are getting somewhere

--- app/src/main/java/com/adellica/thumbkeyboard/ThumbJoy.java
+++ app/src/main/java/com/adellica/thumbkeyboard/ThumbJoy.java
@@ -11,23 +11,20 @@ import java.io.IOException;
 import java.lang.Runnable;
 
 public class ThumbJoy {
-
-    public interface Code {
-        Obj pop();
-        void push(Obj o);
+    public interface Env {
+        public Object get(Object key);
     }
-
-    public interface Obj {
-        void exe(Machine m);
+    public interface JoyMachine {
+        public Pair eval(Pair stk);
     }
-
+    
     public static class TFE extends RuntimeException {
         public TFE() {}
         public TFE(String m) {super(m);}
     }
     public static class EmptyStackPopped extends TFE {}
     public static class TypeMismatch extends TFE {
-        public TypeMismatch(Class expected, Obj got) {
+        public TypeMismatch(Class expected, Object got) {
             super("expected " + expected.getSimpleName() + ", got " + got);
         }
     }
@@ -37,230 +34,60 @@ public class ThumbJoy {
         }
     }
 
-    abstract public static class Datum<T> implements Obj {
+    public static class Pair implements JoyMachine {
+        public final Object car;
+        public final Pair cdr;
+        public static Pair nil = new Pair(null, null);
+        public Pair(Object car, Pair cdr) { this.car=car; this.cdr=cdr;}
+        public static Pair cons(Object car, Pair cdr) { return new Pair(car, cdr); }
+        public static Pair list(Object... args) {
+            Pair p = Pair.nil;
+            for(int i = args.length - 1; i >= 0 ; i--) {
+                p = cons(args[i], p);
+            }
+            return p;
+        }
+
+        public String toString() {
+            if(this == Pair.nil) return "[]";
+            String f = "[";
+            Pair p = this;
+            while(p != nil) {
+                f += " " + p.car;
+                p = p.cdr;
+            }
+            return f + " ]";
+        }
+        
+        public Pair eval(Pair stk) {
+            Pair p = this;
+            while(p != Pair.nil) {
+                stk = run(p.car, stk);
+                p = p.cdr;
+            }            
+            return stk;
+        }
+    }
+
+    abstract public static class Datum<T> {
         final T value;
-        public Datum(T value) {
-            this.value = value;
-        }
-        public void exe(Machine m) {
-            // default behaviour is self-evaluation
-            m.stack.push(this);
-        }
+        public Datum(T value) { this.value = value; }
         public boolean equals(Object o) {
             if(o == null) return false;
             if(!(this.getClass().equals(o.getClass()))) return false;
-
             return value.equals(((Datum)o).value);
-        }
-        abstract public String toString();
-    }
-
-    public static class Keyword extends Datum<String> {
-        public Keyword(String v) { super(v); }
-        public String toString() {
-            return ":" + value;
-        }
-    }
-    public static class Str extends Datum<String> {
-        public Str(String v) { super(v); }
-        public String toString() {
-            return "\"" + value + "\"";
-        }
-    }
-    public static class Boolean extends Datum<java.lang.Boolean> {
-        public Boolean(boolean b) { super(b); }
-        public String toString() {
-            return value.booleanValue() ? "?t" : "?f";
         }
     }
     public static class Word extends Datum<String> {
-        public Word(String v) { super(v); }
-        public String toString() {
-            return value;
-        }
-        public void exe(Machine m) {
-            Obj ref = m.dict.get(value);
-            if(ref == null) throw new InvalidReference(value);
-            ref.exe(m);
-        }
+        public Word(String s) { super(s); }
+        public String toString() { return value.toString(); }
     }
-    public static class Lst extends Datum<List<Obj>> {
-        // s here should be in reverese order!
-        public Lst(List<Obj> s) { super(s); }
-        public String toString() {
-            return value.toString();
-        }
-    }
-
-    public static class CodeBlock extends Datum<List<Obj>> {
-        public CodeBlock(List<Obj> v) { super(v); }
-        public String toString() {
-            return '@' + value.toString();
-        }
-        public void exe(Machine m) {
-            m.push(new Lst(value));
-        }
-    }
-
-    // InputStream combined with a stack so we can push data to it.
-    public static class InputCode implements Code {
-        final InputStream is;
-        public InputCode(InputStream is) {
-            this.is = is;
-        }
-        Stack<Obj> stack = new Stack<Obj>();
-        public Obj pop() {
-            if(!stack.isEmpty()) return stack.pop();
-            return Reader.read(is);
-        }
-        public void push(Obj obj) {
-            stack.push(obj);
-        }
-    }
-
-    public static class StackCode implements Code {
-        final Stack<Obj> stk;
-        public StackCode(Stack<Obj> stk) {this.stk = stk;}
-        public Obj pop() {
-            if(stk.isEmpty()) return null;
-            return stk.pop();
-        }
-        public void push(Obj obj) {stk.push(obj);}
-    }
-
-    
-
-    public static class Machine {
-        public Stack<Obj> stack = new Stack<Obj>();
-        Map<String, Obj> dict = new HashMap<String, Obj>();
-        private Code code;
-
-        public void push(Obj o) { stack.push(o); }
-
-        @SuppressWarnings("unchecked")
-        public <T> T pop(Class<T> t) {
-            if(stack.isEmpty()) throw new EmptyStackPopped();
-            Obj r = stack.pop();
-            if(t.isInstance(r)) return (T)r;
-            throw new TypeMismatch(t, r);
-        }
-
-        public Code code() { return code; }
-        public void run(InputStream is) { run(new InputCode(is)); }
-        public void run(Code code) {
-            this.code = code;
-            while (true) {
-                try {
-                    System.out.println("popping");
-                    Obj op = code.pop();
-                    if (op == null) break;
-                    op.exe(this);
-                } catch (TFE e) {
-                    System.err.println("obs: " + e.getClass().getSimpleName()
-                            + (e.getMessage() == null ? "" : " " + e.getMessage()));
-                }
-            }
-        }
-        public void exe(Obj o) { o.exe(this); }
-
-        public Machine() {
-            dict.put("if", new Obj() {
-                public void exe(Machine m) {
-                    final Code code = m.code();
-                    final Obj c0 = code.pop();
-                    final Obj c1 = code.pop();
-                    if(m.pop(Boolean.class).equals(new Boolean(true))) {
-                        code.push(c0);
-                    } else {
-                        code.push(c1);
-                    }
-                }
-                public String toString() { return "_IF"; }
-            });
-            dict.put("quote", new Obj() {
-                public void exe(Machine m) {
-                    final Obj c0 = m.code.pop();
-                    m.push(c0);
-                }
-                public String toString() { return "_QUOTE"; }
-            });
-            dict.put("drop", new Obj() {
-                public void exe(Machine m) {
-                    m.pop(Obj.class);
-                }
-                public String toString() { return "_DROP"; }
-            });
-            dict.put("swap", new Obj() {
-                public void exe(Machine m) {
-                    final Obj o0 = m.pop(Obj.class);
-                    final Obj o1 = m.pop(Obj.class);
-                    m.push(o0);
-                    m.push(o1);
-                }
-                public String toString() { return "_SWAP"; }
-            });
-            dict.put("exe", new Obj() {
-                public void exe(Machine m) {
-                    m.pop(Obj.class).exe(m);
-                }
-                public String toString() { return "_EXE"; }
-            });
-            dict.put("i", new Obj() {
-                public void exe(Machine m) {
-                    final Lst code = m.pop(Lst.class);
-                    m.run(new StackCode(l2s(code)));
-                }
-                public String toString() { return "_EXE"; }
-            });
-            dict.put("map", new Obj() {
-                public void exe(Machine m) {
-                    final Lst code = m.pop(Lst.class);
-                    final Lst lst  = m.pop(Lst.class);
-                    m.run(new StackCode(l2s(code)));
-                }
-                public String toString() { return "_EXE"; }
-            });
-            dict.put("save", new Obj() {
-                public void exe(Machine m) {
-                    final Obj value = m.pop(Obj.class);
-                    final Word variable = m.pop(Word.class);
-                    m.dict.put(variable.value, value);
-                }
-                public String toString() { return "_SAVE"; }
-            });
-            dict.put("d", new Obj() {
-                public void exe(Machine m) {
-                    m.stack.push(new Str(dict.toString()));
-                }
-                public String toString() { return "_D"; }
-            });
-        }
-
-    }
-
-    public static Stack<Obj> l2s(Lst source) {
-        final Stack<Obj> source = new Stack<Obj>();
-        final List<Obj> lst = source.value;
-        for(int i = lst.size()-1 ; i >= 0 ; i--) {
-            source.push(lst.get(i));
-        }
-        return source;
-    }
-    
-    public static void main(String[] args) {
-        final Machine m = new Machine();
-
-        final InputStream pis = new InputStreamEnterCallback(System.in, new Runnable() {
-            public void run() {
-                System.out.print(m.stack + " ");
-            }
-        });
-
-        m.run(pis);
+    public static class Keyword extends Datum<String> {
+        public Keyword(String s) { super(s); }
+        public String toString() { return ":" + value; }
     }
 
     public static class Reader {
-
         public static boolean isWs(int c) {
             switch(c) {
                 case ' ': case '\t':
@@ -268,35 +95,21 @@ public class ThumbJoy {
                 default: return false;
             }
         }
-        public static boolean isDelimiter(int c) {
-            if(isWs(c)) return true;
-            switch(c) {
-                case '(': case ')': case '[': case ']': return true;
-                default: return false;
-            }
-        }
         // dummy token for ')'
-        private static Obj CLOSE_PAREN = new Obj() {
-            public void exe(Machine M) { throw new RuntimeException("wtf2");}
-        };
-
+        private static Object CLOSE_PAREN = new Object() {};
         // read a list of instructions (  :a :b drop )
-        public static Obj readCodeBlock(InputStream is) {
-            final List<Obj> code = new ArrayList<Obj>();
-            int levels = 0;
+        public static Pair readCodeBlock(InputStream is) {
+            Pair lst = Pair.nil;
             while(true) {
-                Obj o = read(is);
+                Object o = read(is);
                 if(o == null) break;
-                if(o == CLOSE_PAREN) {
-                    levels--;
-                    if(levels < 0) break;
-                }
-                code.add(o);
+                if(o == CLOSE_PAREN) break;
+                lst = Pair.cons(o, lst);
             }
-            return new CodeBlock(code);
+            return lst;
         }
 
-        public static Obj read(InputStream is) {
+        public static Object read(InputStream is) {
             int c;
             try {
                 while((c = is.read()) >= 0) {
@@ -350,4 +163,21 @@ public class ThumbJoy {
         }
     }
 
+    public static Pair run(Object o, Pair stk) {
+        if(o instanceof JoyMachine) return ((JoyMachine)o).eval(stk);
+        return Pair.cons(o, stk);
+    }
+
+    public final static JoyMachine _PLUS = new JoyMachine() {
+            public Pair eval(Pair stk) {
+                Integer i0 = (Integer)stk.car;
+                Integer i1 = (Integer)stk.cdr.car;
+                return Pair.cons(i0 + i1, stk.cdr.cdr);
+            }
+        };
+
+    public static void main(String[] args) {
+        System.out.println("hello " + Pair.nil);
+        System.out.println(run(Pair.list(2, 3, _PLUS), Pair.nil));
+    }
 }
