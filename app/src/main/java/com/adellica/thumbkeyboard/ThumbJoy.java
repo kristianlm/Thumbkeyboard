ba4f1e01141917b539e867de8c364b93e948510c
Kristian Lein-Mathisen <kristianlein@gmail.com>
2017-10-20 17:01:59 +0200

    I think I've got the API now
    the Applicable inteface is everything, really. it needs 3 values,
    because:
    
    - PER INVOKATION: we want to be able to modify the "current execution"
      environment (e.g. QUOTE needs to be able to POP the next token)
    
    - PER INVOKATION: stack. each invocation gets its own stack to work
      with. these stacks are immutable. but it makes it harder to share it (IPairBox)
    
    - Machine: this is the only thing that's shared between different
      repls. that's nice. and yes, that needs to be shared because we need
      to be able to define "handle" in our repl, and have the UI thread
      execute it

--- app/src/main/java/com/adellica/thumbkeyboard/ThumbJoy.java
+++ app/src/main/java/com/adellica/thumbkeyboard/ThumbJoy.java
@@ -7,16 +7,13 @@ import java.util.HashMap;
 
 import static com.adellica.thumbkeyboard.ThumbJoy.Pair.cons;
 import static com.adellica.thumbkeyboard.ThumbJoy.Pair.list;
-import static com.adellica.thumbkeyboard.ThumbReader.QUOTE;
+
 
 public class ThumbJoy {
-    public interface Env {
-        Object get(Object key);
-    }
     public interface Applicable {
-        IPair exe(IPair stk, Machine m);
+        IPair exe(IPair stk, Machine m, MutablePair code);
     }
-    
+
     public static class TFE extends RuntimeException {
         public TFE() {}
         public TFE(String m) {super(m);}
@@ -40,24 +37,43 @@ public class ThumbJoy {
         IPair cdr();
         <T> T car(Class<T> t);
     }
-    public static class Pair implements IPair, Applicable {
+    abstract public static class APair implements IPair, Applicable {
+
+        @SuppressWarnings("unchecked")
+        public <T> T car(Class<T> t) {
+            if(t.isInstance(car())) return (T)car();
+            throw new TypeMismatch(t, car());
+        }
+        public String toString() {
+            if(this == Pair.nil) return "[ ]";
+            String f = "[";
+            IPair p = this;
+            while(p != Pair.nil) {
+                f += " " + p.car();
+                p = p.cdr();
+            }
+            return f + " ]";
+        }
+
+        @Override
+        public IPair exe(IPair stk, Machine m, MutablePair code) {
+            //MutablePair recode = new MutablePair(this);
+
+            return m.run(this, stk);
+        }
+    }
+    public static class Pair extends APair implements Applicable {
         private final Object __car;
         private final IPair __cdr;
         public Pair(Object car, IPair cdr) { __car=car; __cdr=cdr;}
         public Object car() { return __car; }
         public IPair cdr() { return __cdr; }
 
-        @SuppressWarnings("unchecked")
-        public <T> T car(Class<T> t) {
-            if(t.isInstance(__car)) return (T)__car;
-            throw new TypeMismatch(t, __car);
-        }
-            
         public static final Pair nil = new Pair(null, null) {
                 public <T> T car(Class<T> t) { throw new EmptyStackPopped(); }
                 public Object car() { throw new EmptyStackPopped(); }
                 public Pair cdr() { throw new EmptyStackPopped(); }
-                public IPair exe(IPair stk, Machine m) {return this;}
+                public IPair exe(IPair stk, Machine m, MutablePair code) {return this;}
         };
 
         public static Pair cons(Object car, IPair cdr) { return new Pair(car, cdr); }
@@ -68,44 +84,30 @@ public class ThumbJoy {
             }
             return p;
         }
-        public String toString() {
-            if(this == nil) return "[ ]";
-            String f = "[";
-            IPair p = this;
-            while(p != nil) {
-                f += " " + p.car();
-                p = p.cdr();
-            }
-            return f + " ]";
+    }
+    public static class MutablePair {
+        IPair p;
+        public MutablePair(IPair p) {
+            this.p = p;
         }
-
-        @Override
-        public IPair exe(IPair stk, Machine m) {
-            System.out.println("EXECUTING PAIR");
-            IPair p = this;
-            while (p != nil) {
-                if(p.car() == QUOTE) {
-                    System.out.println("quoting cdr " + p.cdr());
-                    stk = cons(p.cdr().car(), stk);
-                    System.out.println("quoting after  " + stk);
-                    p = p.cdr().cdr();
-                    continue;
-                }
-                stk = m.eval(p.car(), stk);
-                p = p.cdr();
-            }
-            return stk;
+        public boolean isEmpty() {
+            return p == Pair.nil;
+        }
+        public Object pop() {
+            Object o = p.car();
+            p = p.cdr();
+            return o;
         }
     }
 
     abstract public static class Datum<T> {
         final T value;
         public Datum(T value) { this.value = value; }
-        public boolean equals(Object o) {
-            if(o == null) return false;
-            if(!(this.getClass().equals(o.getClass()))) return false;
-            return value.equals(((Datum)o).value);
-        }
+        // public boolean equals(Object o) {
+        //     if(o == null) return false;
+        //     if(!(this.getClass().equals(o.getClass()))) return false;
+        //     return value.equals(((Datum)o).value);
+        // }
     }
     public static class Keyword extends Datum<String> {
         public Keyword(String s) { super(s); }
@@ -114,10 +116,10 @@ public class ThumbJoy {
     public static class Word extends Datum<String> implements Applicable {
         public Word(String s) { super(s);}
         public String toString() { return value; }
-        public IPair exe(IPair stk, Machine m) {
+        public IPair exe(IPair stk, Machine m, MutablePair code) {
             Object o = m.dict.get(this.value);
             if(o == null) throw new InvalidReference(this.toString());
-            return m.eval(o, stk);
+            return m.eval(o, stk, code);
         }
     }
 
@@ -131,7 +133,9 @@ public class ThumbJoy {
     }
     
     public static class Machine {
+        //public IPair stk = Pair.nil;
         public Map<Object, Object> dict = new HashMap<Object, Object>();
+        public Map<String, Object> macros = new HashMap<String, Object>();
 
         @SuppressWarnings("unchecked")
         public <T> T get(String key, Class<T> t) {
@@ -140,44 +144,41 @@ public class ThumbJoy {
             throw new TypeMismatch(t, ref);
         }
         public Machine() {
-            new ApplicableCore("conc", this) {
-                    public IPair exe(IPair stk, Machine m) {
-                        Object e0 = stk.car();
-                        Object e1 = stk.cdr().car();
-                        return cons(e0.toString() + e1, stk.cdr().cdr());
-                    }
-                };
             new ApplicableCore("drop", this) {
-                    public IPair exe(IPair stk, Machine m) {
+                    public IPair exe(IPair stk, Machine m, MutablePair code) {
                         return stk.cdr();
                     }
                 };
             new ApplicableCore("dup", this) {
-                    public IPair exe(IPair stk, Machine m) {
+                    public IPair exe(IPair stk, Machine m, MutablePair code) {
                         return cons(stk.car(), stk);
                     }
                 };
-            new ApplicableCore("d", this) {
-                public IPair exe(IPair stk, Machine m) {
-                    return cons(dict, stk);
-                }
-            };
             new ApplicableCore("dd", this) {
-                public IPair exe(IPair stk, Machine m) {
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
                     Object o = dict.get("println");
                     if(o == null) throw new InvalidReference("println");
-                    Machine.this.eval(o, list(dict));
+                    Machine.this.eval(o, list(dict), code);
                     return stk;
                 }
             };
             new ApplicableCore("i", this) {
-                public IPair exe(IPair stk, Machine m) {
-                    return m.eval(stk.car(), stk.cdr());
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
+                    return m.eval(stk.car(), stk.cdr(), code);
+                }
+            };
+            new ApplicableCore("'", this) {
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
+                    if(code.isEmpty())
+                        throw new RuntimeException("QUOTE: unexpected eof");
+
+                    //System.out.println("QUOTing " + m.next(Object.class));
+                    return cons(code.pop(), stk);
                 }
             };
             new ApplicableCore("set", this) {
                 @Override
-                public IPair exe(IPair stk, Machine m) {
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
                     final Word name = stk.cdr().car(Word.class);
                     final Object content = stk.car();
                     dict.put(name.value, content);
@@ -186,14 +187,14 @@ public class ThumbJoy {
             };
             new ApplicableCore("get", this) {
                 @Override
-                public IPair exe(IPair stk, Machine m) {
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
                     final Keyword name = stk.car(Keyword.class);
                     return cons(dict.get(name.value), stk.cdr());
                 }
             };
             new ApplicableCore("println", this) {
                 @Override
-                public IPair exe(IPair stk, Machine m) {
+                public IPair exe(IPair stk, Machine m, MutablePair code) {
                     OutputStream os = m.get("out", OutputStream.class);
                     try {
                         os.write((stk.car().toString() + "\n").getBytes());
@@ -207,10 +208,23 @@ public class ThumbJoy {
                 }
             };
         }
-        public IPair eval(Object o, IPair stk) {
-            if (o instanceof Applicable) return ((Applicable) o).exe(stk, this);
+
+        public IPair eval(final Object o, IPair stk, MutablePair mp) {
+            if (o instanceof Applicable) return ((Applicable) o).exe(stk, this, mp);
             return cons(o, stk); // defaults to "self evaluation"
         }
-
+        public IPair run(IPair _source, IPair stk) {
+            MutablePair mp = new MutablePair(_source);
+            while(!mp.isEmpty()) {
+                try {
+                    final Object read = mp.pop();
+                    stk = eval(read, stk, mp);
+                } catch (ThumbJoy.TFE e) {
+                    //e.printStackTrace();
+                    System.out.println("error: " + e);
+                }
+            }
+            return stk;
+        }
     }
 }
---
+++ app/src/main/java/com/adellica/thumbkeyboard/ThumbReader.java
@@ -0,0 +1,253 @@
+package com.adellica.thumbkeyboard;
+
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.IOException;
+import java.lang.Runnable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.adellica.thumbkeyboard.ThumbJoy.Applicable;
+import com.adellica.thumbkeyboard.ThumbJoy.IPair;
+import com.adellica.thumbkeyboard.ThumbJoy.Pair;
+import com.adellica.thumbkeyboard.ThumbJoy.Keyword;
+import com.adellica.thumbkeyboard.ThumbJoy.Word;
+import com.adellica.thumbkeyboard.ThumbJoy.Machine;
+
+import static com.adellica.thumbkeyboard.ThumbJoy.Pair.cons;
+
+
+public class ThumbReader {
+
+    public static boolean isWs(int c) {
+        switch(c) {
+        case ' ': case '\t':
+        case '\r': case '\n': return true;
+        default: return false;
+        }
+    }
+    // dummy token for ')'
+    private static Object CLOSE_PAREN = new Object() {};
+
+    public static class Quoted implements Applicable {
+        final Object obj;
+        public Quoted(Object obj) { this.obj = obj; }
+        @Override public IPair exe(IPair stk, Machine m, ThumbJoy.MutablePair code) { return cons(obj, stk); }
+        @Override public String toString() { return "“" + obj.toString(); }
+    }
+
+    // public static class _EVIL implements Applicable {
+    //     public IPair exe(IPair stk, Machine m) {
+    //         throw new ThumbJoy.TFE(){public String getMessage() {return "you should never eval me";}};
+    //     }
+    //     public String toString() {return "“"; }
+    // }
+    // public static final _EVIL QUOTE = new _EVIL();
+
+    final InputStream is;
+    //final ReaderMachine m = new ReaderMachine();
+    public ThumbReader(InputStream is) {
+        this.is = is;
+    }
+
+    // public class ReaderMachine extends Machine {
+    //     public ReaderMachine(IPair source) {
+    //         super(source);
+    //         //dict.clear();
+    //         new ApplicableCore("read", this) {
+    //             public IPair exe(IPair stk, Machine m) {
+    //                 final Object r = read();
+    //                 System.out.println("running read " + r);
+    //                 return cons(r, stk);
+    //             }
+    //         };
+    //     }
+    // }
+
+    public static class IPairReader extends ThumbJoy.APair {
+        final ThumbReader tr;
+        Object o = null;
+        public IPairReader(ThumbReader tr) {
+            this.tr = tr;
+        }
+        public Object car() {
+            if(o == null) {
+                o = tr.read();                
+            }
+            return o;
+        }
+        public IPair cdr() {
+            return new IPairReader(tr);
+        }
+        @Override public String toString() { return "<"+car()+"...>"; }
+        @Override
+        public IPair exe(IPair stk, Machine m, ThumbJoy.MutablePair code) { return stk; }
+    }
+
+    public Pair reverse(IPair pair) {
+        Pair result = Pair.nil;
+        while(pair != Pair.nil) {
+            result = cons(pair.car(), result);
+            pair = pair.cdr();
+        }
+        return result;
+    }
+
+    public Object read() {
+        int c;
+        try {
+            while((c = is.read()) >= 0) {
+                if(isWs(c)) continue;
+                switch(c) {
+                case ':': return new Keyword(readUntilWS(""));
+                case '?': return new Boolean("t".equals(readUntilWS("")));
+                case ']': return CLOSE_PAREN;
+                case '[': return readQuotedList();
+                default:
+                    final String name = readUntilWS(String.format("%c", c));
+                    //final Object macro = m.dict.get(name);
+                    //if("'".equals(name))
+                    //  return QUOTE;
+                    //else if("def".equals(name)) {
+                        // final Object res = (new Quoted(reverse(m.eval(
+                        //         list(
+                        //                 QUOTE, QUOTE,
+                        //                 new Word("read"),
+                        //                 new Word("read"),
+                        //                 QUOTE, new Word("set")//, new Keyword("test")
+                        //         ),
+                        //         Pair.nil))));
+                        // System.out.println("def produced: " + res);
+                        // return res;
+                    //}
+                    //if(macro == null)
+                        return new Word(name);
+                    //return
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // read a list of instructions [ :a :b drop ]
+    // note that we don't have syntax for reading non-quoted lists! because that would be completely useless.
+    public Quoted readQuotedList() {
+        List<Object> lst = new ArrayList<Object>();
+        while(true) {
+            Object o = read();
+            if(o == null) break;
+            if(o == CLOSE_PAREN) break;
+            lst.add(o);
+        }
+        Pair p = Pair.nil;
+        for(int i = lst.size() - 1; i >= 0 ; i--) {
+            p = cons(lst.get(i), p);
+        }
+        return new Quoted(p);
+    }
+
+    public String readUntilWS (String init) throws IOException {
+        int c;
+        while((c = is.read()) >= 0) {
+            if(isWs(c)) break;
+            init += String.format("%c", c);
+        }
+        return init;
+    }
+
+
+    /**
+       show user prompt only on ENTER keys for less noise
+    */
+    public static class InputStreamEnterCallback extends InputStream {
+        final InputStream source;
+        final Runnable callback;
+        public InputStreamEnterCallback(InputStream source, Runnable callback) {
+            this.source = source;
+            this.callback = callback;
+        }
+        private boolean showPrompt = true;
+        public int read() throws IOException {
+            if(showPrompt) {
+                callback.run();
+                showPrompt = false;
+            }
+            final int c = source.read();
+            if(c == '\n') showPrompt = true;
+            return c;
+        }
+        public void close() throws IOException {
+            source.close();
+        }
+    }
+
+    public static class IPairBox { public IPair stk = Pair.nil; }
+    public static void repl(final Machine m, InputStream is, OutputStream out2) {
+        final PrintStream out = new PrintStream(out2);
+        final IPairBox box = new IPairBox();
+        InputStream pis = new InputStreamEnterCallback(is, new Runnable() {
+                public void run() {
+                    out.print(box.stk + " ");
+                }
+            });
+
+        m.dict.put("out", out); // obs: overwrites all clients
+
+        ThumbReader reader = new ThumbReader(pis);
+        IPair source = new IPairReader(reader);
+        ThumbJoy.MutablePair mp = new ThumbJoy.MutablePair(source);
+
+        while(!mp.isEmpty()) {
+            try {
+                final Object read = mp.pop();
+                box.stk = m.eval(read, box.stk, mp);
+            } catch (ThumbJoy.TFE e) {
+                //e.printStackTrace();
+                System.out.println("error: " + e);
+            }
+        }//        m.run(source, Pair.nil);
+    }
+
+    public static void serve(final ThumbJoy.Machine m, int port) {
+        try {
+            final ServerSocket ss = new ServerSocket(port);
+            final Socket s = ss.accept();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final PrintStream pw = new PrintStream(s.getOutputStream());
+                        pw.println("Welcome to Kristians silly ThumbForth");
+
+                        ThumbReader.repl(m, s.getInputStream(), pw);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String [] args) {
+        final int port = 2345;
+        System.out.println("\u001b[32mThumbReader\u001b[0m REPL on port " + port);
+        final Machine m = new Machine();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                serve(m, port);
+            }
+            }).start();
+        repl(m, System.in, System.out);
+    }
+}
